f6f8a07187df2cb6d35c8712bf1d60f413dcd9cb
animesh singh <animeshsingh692@gmail.com>
2023-08-21 13:23:46 +0530

    Banner and Search Exercise design is complete

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'
+import './App.css'
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import ExerciseDetails from './Pages/ExerciseDetails';
+import Footer from './Components/Footer';
+
+const App = () => {
+    return (
+        <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/exercise/:id' element={<ExerciseDetails />} />
+            </Routes>
+            <Footer />
+        </Box>
+    )
+}
+
+export default App;
---
+++ src/utils/FetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '33b06b7932msh8e71a2054cdeae4p160e74jsn544c9931d50a'
+        ,
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+}
